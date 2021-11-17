40248400a3a0c82f064bd42fd6acc72ea4274662
Angelo Pellegrino <angelo.pellegrino@it.abb.com>
2021-11-17 15:24:19 +0100

    add

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox_hit = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Yellow;
+            this.textBox1.Location = new System.Drawing.Point(324, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Pettole";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(489, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(159, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Bombette";
+            // 
+            // textBox_hit
+            // 
+            this.textBox_hit.BackColor = System.Drawing.Color.DarkGray;
+            this.textBox_hit.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_hit.Location = new System.Drawing.Point(12, 149);
+            this.textBox_hit.Multiline = true;
+            this.textBox_hit.Name = "textBox_hit";
+            this.textBox_hit.Size = new System.Drawing.Size(1038, 149);
+            this.textBox_hit.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 80);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 45);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(814, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "IP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(1062, 344);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox_hit);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Quizzellone ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox_hit;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        UDPSocket server = new UDPSocket();
+
+
+        Boolean ServerOn = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static string GetLocalIPAddress()
+        {
+      
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            return host.AddressList[1].ToString();
+
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            ServerOn = true;
+            server.Server("192.168.0.119", 27000);
+            timer1.Enabled = true;
+            button1.Text = "Start";
+            label1.Text = GetLocalIPAddress();
+        
+        }
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            if(MSGmes.getset_new)
+            {
+                MSGmes.getset_new = false;
+                if(MSGmes.get_set_valueres == 1)
+                {
+                    textBox_hit.Text = textBox1.Text;
+                    textBox_hit.BackColor = textBox1.BackColor;
+                }
+                else if(MSGmes.get_set_valueres == 2)
+                {
+                     textBox_hit.Text = textBox2.Text;
+                     textBox_hit.BackColor = textBox2.BackColor;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            MSGmes.getset_new = false;
+            MSGmes.getset_locke = false;
+            textBox_hit.Text = "pronti....";
+            textBox_hit.BackColor = Color.DarkGray;
+            
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            ServerOn = true;
+            
+            timer1.Enabled = false;
+            button1.Text = "START";
+            textBox_hit.Text = "";
+        }
+
+    }
+
+
+     class MSGmes
+    {
+        private static Boolean neew;
+        private static Boolean locke;
+        private static int valueres;
+
+        public static Boolean getset_new
+        {
+            get { return neew; }
+            set { neew = value; }
+        }
+          public static Boolean getset_locke
+        {
+            get { return locke; }
+            set { locke = value; }
+        }
+
+        public static int get_set_valueres
+        {
+            get { return valueres; }
+            set { valueres = value; }
+        }
+    }
+
+    public class UDPSocket
+    {
+        private Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private const int bufSize = 8 * 1024;
+        private State state = new State();
+        private EndPoint epFrom = new IPEndPoint(IPAddress.Any, 0);
+        private AsyncCallback recv = null;
+        MSGmes result = new MSGmes();
+
+        public class State
+        {
+            public byte[] buffer = new byte[bufSize];
+        }
+
+        public void Server(string address, int port)
+        {
+            _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, true);
+            _socket.Bind(new IPEndPoint(IPAddress.Parse(address), port));
+            Receive();
+        }
+
+        public void Client(string address, int port)
+        {
+            _socket.Connect(IPAddress.Parse(address), port);
+            Receive();
+        }
+
+        public void Send(string text)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            _socket.BeginSend(data, 0, data.Length, SocketFlags.None, (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndSend(ar);
+               
+            }, state);
+        }
+
+        private void Receive()
+        {
+            _socket.BeginReceiveFrom(state.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv = (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndReceiveFrom(ar, ref epFrom);
+                _socket.BeginReceiveFrom(so.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv, so);
+                if(MSGmes.getset_locke == false)
+                {
+                    MSGmes.getset_locke = true;
+                    MSGmes.getset_new = true;
+                    MSGmes.get_set_valueres = Convert.ToInt32(so.buffer[0]);
+                }
+               
+                Console.WriteLine("RECV: {0}: {1}, {2}", epFrom.ToString(), bytes, Encoding.ASCII.GetString(so.buffer, 0, bytes));
+            }, state);
+           
+          
+        }
+    }
+
+    
+}
